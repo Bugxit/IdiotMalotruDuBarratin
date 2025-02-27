3af7f6ae2bb60861b863f2c356a9f9e5f4470660
Funasitien <funasitien@dreamclouds.fr>
2025-02-27 15:37:02 +0100

    Database plus précis

--- operations.py
+++ operations.py
@@ -4,9 +4,16 @@ import mysql.connector
 class DBBIGBOSS:
     def __init__():
         self.conn = mysql.connector.connect(host="localhost",user="elevelocal", database="imdb")
-    def getMovieData(movie):
+    def getMovieDataByName(name):
         cursor = self.conn
-        cursor.execute("SELECT originalTitle, startYear, numVotes FROM work_basics JOIN work_ratings USING (id_work) WHERE worktype LIKE 'movie' AND originalTitle = '?' ORDER BY numVotes DESC LIMIT 10;", (movie))
+        cursor.execute("SELECT originalTitle, startYear, numVotes FROM work_basics JOIN work_ratings USING (id_work) WHERE worktype LIKE 'movie' AND originalTitle = '?' ORDER BY numVotes DESC LIMIT 10;", (name))
+        lignes = cursor.fetchall()
+        for ligne in lignes:
+            print(ligne)
+        conn.close()
+    def getMovieData(idée):
+        cursor = self.conn
+        cursor.execute("SELECT originalTitle, startYear, numVotes FROM work_basics JOIN work_ratings USING (id_work) WHERE worktype LIKE 'movie' AND idWork = '?' ORDER BY numVotes DESC LIMIT 10;", (idée))
         lignes = cursor.fetchall()
         for ligne in lignes:
             print(ligne)
