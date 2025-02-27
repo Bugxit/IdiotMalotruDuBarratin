9c7ceed4649e4ad7ed9c1691480fa3d8e3b3f8c6
Bugxit <101186137+Bugxit@users.noreply.github.com>
2025-02-27 13:45:26 +0100

    Create dessin.py

---
+++ dessin.py
@@ -0,0 +1,62 @@
+from turtle import *
+from os import remove
+
+wn = Screen()
+wn.tracer(0)
+
+def bouge(x, y):
+    """
+        Permet de déplacer la tortue à un point donné
+    """
+    up()
+    goto(x,y)
+    down()
+
+def offset(xoff, yoff):
+    cur_x, cur_y = pos()
+    bouge(cur_x + xoff, cur_y + yoff)
+
+def ligne(x1, y1, x2, y2):
+    """
+        Trace une ligne entre les deux points (x1, y1) et (x2, y2)
+    """
+    bouge(x1, y1)
+    goto(x2, y2)
+
+def rect(x, y, width, height):
+    """
+        Trace un rectangle de coin haut gauche (x, y),
+        de largeur width et de hauteur height.
+    """
+    bouge(x, y)
+    goto(x + width, y)
+    goto(x + width, y + height)
+    goto(x, y + height)
+    goto(x, y)
+
+
+def cercle(x, y, r):
+    """
+        Trace un cercle de centre (x, y) et de rayon r
+    """
+
+    bouge(x, y-r)
+    circle(r)
+
+def ecrire(x, y, texte, align='left', font=('Arial', 32, 'normal')):
+    """
+        Écrit les texte aux coordonnées x et y.
+        (x,y) devra être le point en bas à gauche du texte.
+    """
+    bouge(x, y)
+    write(texte, align=align, font=font)
+
+def save_image(output="output.png"):
+    from PIL import Image
+
+    wn.update()
+    getscreen().getcanvas().postscript(file='funaaimelesfraises.ps')
+    image = Image.open("funaaimelesfraises.ps")
+    image.convert("RGB").save("output.jpg", "JPEG")
+    image.save("output.png", "PNG")
+    remove("funaaimelesfraises.ps")
