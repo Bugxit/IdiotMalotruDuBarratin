4d94643828f36c9c0fc6132b9377100476c78a5f
Funasitien <funasitien@dreamclouds.fr>
2025-03-06 14:46:28 +0100

    d

--- operations.py
+++ operations.py
@@ -22,7 +22,7 @@ class DBBIGBOSS:
         query = "\
         SELECT work_episode.seasonNumber, work_ratings.averageRating, work_akas.title\
         FROM work_basics JOIN work_episode ON work_basics.id_work = work_episode.id_work_parent\
-        JOIN work_ratings ON work_ratings.id_work = work_episode.id_work_parent\
+        JOIN work_ratings ON work_ratings.id_work = work_episode.id_work\
         JOIN work_akas ON work_akas.id_work = work_episode.id_work\
         WHERE worktype LIKE 'tvSeries' AND work_akas.isOriginalTitle = 1 AND work_basics.id_work = %s\
         ORDER BY work_episode.seasonNumber, work_episode.episodeNumber\
