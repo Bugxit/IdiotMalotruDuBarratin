5e7197641002c2080dd60a77ff24d32a3f33030e
Bugxit <101186137+Bugxit@users.noreply.github.com>
2025-03-06 15:23:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import logic
 import operations
 
+import sys
+print (sys.argv)
+
 if __name__ == "__main__":
-    path = "/var/www/html/images/"
+    argc = len(sys.argv)
+    if argc < 2: exit(1)
+    
+    id = sys.argv[1]
+
+    path = f"/var/www/html/images/{id}"
+    
+    query_res = operations.db.episodeList(id)
+    name, rating = logic.format_ratings_name(query_res)
+    logic.generate_image_series(rating, name, path)
+    
